1b0e903dc39faad88bea29d0edd1e2eed27bfe86
TheTeaCup <lw0482352@gmail.com>
2025-01-21 19:22:13 -0500

    going through the first set of slides

---
+++ BMI.py
@@ -0,0 +1,7 @@
+# BMI Calculator
+
+weight = float(input("Please enter weight (pounds): "))
+
+height = float(input("Please enter heights (inches): "))
+
+print("BMI is", (weight * 0.453592) / ((height * 0.0254) ** 2))
--- ipo.py
+++ ipo.py
@@ -1,1 +1,6 @@
 # Input, Process, Output
+first = int(input("Enter the first number: "))
+
+second = int(input("Enter the second number: "))
+
+print("The sum is", first + second)
