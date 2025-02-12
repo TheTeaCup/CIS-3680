78de791f815ef394deef2c636bf689d2f683502a
TheTeaCup <lw0482352@gmail.com>
2025-02-12 15:16:04 -0500

    OA #3

--- assignments/OA3.py
+++ assignments/OA3.py
@@ -12,29 +12,30 @@ They would like an application to compute the price of a sign based on the follo
 Create a program to calculate the price of a sign.
 """
 
-# Const price
-PRICE = 30
+def main():
+    # Get input for Sign Material, Text, and Letter Color
+    material = input("Enter sign material (Oak or Pine): ")
+    signText = input("Enter sign text: ")
+    letterColor = input("Enter letter color (Black, White, or Gold): ")
 
-# Get input for Sign Material, Text, and Letter Color
-material = input("Enter sign material (Oak or Pine): ")
-signText = input("Enter sign text: ")
-letterColor = input("Enter letter color (Black, White, or Gold): ")
+     # Const price
+    PRICE = 30
 
-# determine material cost
-if material == "Oak":
-    PRICE += 15
-elif material == "Pine":
-    PRICE = PRICE
-else:
-    print("Invaild sign material")
+    # determine material cost
+    if material == "Oak":
+        PRICE += 15
+    elif material == "Pine":
+        PRICE = PRICE
 
-# determine letter cost
+    # determine letter cost
+    if len(signText) > 6:
+        PRICE += (len(signText) - 6) * 3
 
+    # determine letter color cost
+    if letterColor == "Gold":
+        PRICE +=12
 
+    # output
+    print(f"The total cost of the sign is: ${PRICE}")
 
-# determine letter color cost
-
-
-
-# output
-print(f"The total cost of the sign is: ${PRICE}")
+main()
