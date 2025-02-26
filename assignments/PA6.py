b63881740bcfbbc327c63c9c94f0f3a11c03d3c9
TheTeaCup <lw0482352@gmail.com>
2025-02-26 12:58:01 -0500

    working on PA6

---
+++ assignments/PA6.py
@@ -0,0 +1,34 @@
+"""
+PA #6 - Hunter Wilson
+
+The Payroll Department keeps a list of employee information for each pay period in a text file. The format of each line of the file is the following:
+
+<last name> <hourly wage> <hours worked>
+
+Write a program that inputs a filename from the user and prints to the terminal a report of the wages paid to the employees for the given period. The report should be in tabular format with the appropriate header. Each line should contain an employee's name, the hours worked and the wages paid for that period.
+
+Example Run:
+
+Enter the file name: data.txt
+Name            Hours   Total Pay
+Hodges             34      357.00
+Corley             22      159.50
+Hunsinger           5      503.50
+"""
+
+def main():
+    fileName = input("Enter the file name: ")
+    file = open(fileName, 'r')
+
+    print("Name\t\tHours\t\tTotal Pay")
+    for line in file:
+        words = line.split()
+        name = words[0]
+        wage = words[1]
+        hours = words[2]
+        pay = float(wage) * float(hours)
+
+        print(f'{name}\t\t{hours}\t\t{pay:.2f}')
+
+
+main()
