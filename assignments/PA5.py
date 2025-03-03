7d263ab0d912085586c46789ff3a4f97b1cfaed9
TheTeaCup <lw0482352@gmail.com>
2025-03-03 11:56:44 -0500

    list notes and start of PA5

---
+++ assignments/PA5.py
@@ -0,0 +1,13 @@
+"""
+PA #5 - Hunter Wilson
+
+A list is sorted in ascending order if it is empty 
+or each item except the last one is less than or equal to its successor. 
+Define a function called is_sorted that expects a list as an argument and
+returns True if the list is sorted, or returns False otherwise. 
+Include a brief main program to demonstrate the function.
+(Hint: For a list of length 2 or greater, 
+loop through the list and compare pairs of items, 
+from left to right, and return False if the first item in a pair is greater.)
+"""
+
---
+++ examples/lists.py
@@ -0,0 +1,41 @@
+
+myList = [
+    1,
+    2,
+    3,
+    4,
+    5
+]
+
+myList[2:4]
+
+print("12345")
+print(myList)
+
+myList[0] = 2
+print(myList)
+
+myList = [1, 2]
+myList.insert(1, 10)
+print(myList)
+
+myList.insert(3, 25)
+print(myList)
+
+myList = [1, 2]
+myList.append(3)
+print(myList)
+
+myList.extend([11, 12, 13])
+print(myList)
+
+myList.pop()
+print(myList)
+
+myList.pop(0)
+print(myList)
+
+myList = [4, 2, 8, 10]
+myList.sort()
+print(myList)
+
