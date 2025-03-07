aba4b4555c869d85b18bae369cb8601af6087b31
TheTeaCup <lw0482352@gmail.com>
2025-03-06 21:52:29 -0500

    PA5 is done

--- assignments/PA5.py
+++ assignments/PA5.py
@@ -11,3 +11,29 @@ loop through the list and compare pairs of items,
 from left to right, and return False if the first item in a pair is greater.)
 """
 
+def is_sorted(list):
+    # if the list is empty or has 1 item then its 'sorted'
+    if len(list) <= 1:
+        return True
+    
+    # loop through the list
+    for i in range(len(list) - 1):
+        if list[i] > list[i + 1]:
+            return False # if any pair is out of order then its not 'sorted'
+        
+    # otherwise the list is sorted
+    return True
+
+
+def main():
+    # sorted
+    listOne = [1,2,3,4,5,6]
+    print(f"List One is sorted: {is_sorted(listOne)}")
+
+    # not sorted
+    listTwo = [1,3,5,2,4,6]
+    print(f"List Two is sorted: {is_sorted(listTwo)}")
+
+
+if __name__ == "__main__":
+    main()
